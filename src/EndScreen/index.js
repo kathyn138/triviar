e7c6bbe647766249136e504b0308baa7bfa90706
Kathy Nguyen <kathyn262@gmail.com>
2020-10-29 14:56:26 -0700

    added reset quiz functionality

--- src/EndScreen/index.js
+++ src/EndScreen/index.js
@@ -3,15 +3,22 @@ import React from 'react';
 class EndScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    }
+  handleClick(evt) {
+    evt.preventDefault();
+    this.props.resetQuiz();
   }
 
   render() {
     return (
-      "end"
-    )
+      <React.Fragment>
+        <p>Your score is: {this.props.score}</p>
+        <button type="button" className="btn btn-primary" id="start-btn"
+          onClick={this.handleClick}>Start Over</button>
+      </React.Fragment>
+    );
   }
 }
 
